038f226b6af38884901b3cc1d6cd6c59e99ab678
Filip Koza <peacefilip1989@gmail.com>
2021-06-06 14:59:14 +0200

    deleting dev-notes

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const operations = require('./contacts');
-// operations.addContact(
-//   6,
-//   "Abbot Franks",
-//   "scelerisque@magnis.org",
-//   "(186) 568-3720"
-// );
-// operations.removeContact(1);
+
 const argv = require('yargs').argv;
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
@@ -29,6 +23,5 @@ function invokeAction({ action, id, name, email, phone }) {
       console.warn('\x1B[31m Unknown action type!');
   }
 }
-// node index.js--action remove--id = 13
 
 invokeAction(argv);
